9a3ebcadf8b7cdb3737baf37818c276bbfa2b469
Maicou Hahn Fortuna <maicou.fortuna@betha.com.br>
2024-03-08 13:56:40 -0300

    Added the logic to maintain empty spaces and special characteres

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String str = Main.cypherWord("Hello World");
+        String str = Main.cypherWord("Hello World $%¨T&Y");
         System.out.println(str);
        String st2 = Main.cypherWord(str);
         System.out.println(st2);
@@ -13,15 +11,16 @@ public class Main {
     }
     public static String cypherWord(String phrase){
         String alph = "abcdefghijklmnopqrstuvwxyz";
-        String specials =" @#$%^&*()_+{}[]|\\:;\"',.<>?/`~-=";
+        String specials =" @#$%^&*()_+{}[]|\\:;\"',.<>?/`~-=¨";
         Map<Integer,String>mp = new HashMap<>();
         StringBuilder str2 = new StringBuilder();
 
         for (int i = 0; i < alph.length(); i++) {
             mp.put(i+1, String.valueOf(alph.charAt(i)));
         }
+
         for (int i = 0; i < phrase.length(); i++) {
-            
+
             for (Map.Entry<Integer,String> key: mp.entrySet()) {
                 if(key.getValue().equals(String.valueOf(phrase.charAt(i)).toLowerCase())){
                      int count = key.getKey()+13;
@@ -29,9 +28,12 @@ public class Main {
                         count = count-26;
                      }
                      str2.append(mp.get(count));
-                }else if(specials.contains(key.getValue())){
-                            str2.append(key.getValue());
-                        }
+                }
+            }
+            for (int j = 0; j < specials.length(); j++) {
+                if(String.valueOf(phrase.charAt(i)).equals(String.valueOf(specials.charAt(j)))){
+                    str2.append(phrase.charAt(i));
+                }
             }
         }
         return str2.toString();
